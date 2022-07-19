11f3f735def528701558bc2c343f87f6ea9618ea
Smita3434 <sayalig94@gmail.com>
2022-07-19 12:01:24 +0530

    Add project files.

---
+++ EmployeeCrudUsingADO.net/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrudUsingADO.net.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employee name is requied")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Empolyee Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Salary is required")]
+        [DataType(DataType.Text)]
+        [Range(minimum: 1, maximum: 100000)]
+        public  double Salary { get; set; }
+    }
+}
